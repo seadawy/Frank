306dc68f826f84c315dfd07c0204933f866752aa
abdelrhman seadawy <seadawy6452@gmail.com>
2024-01-29 23:00:43 +0200

    JS addQuestions

--- CreateQuiz.php
+++ CreateQuiz.php
@@ -12,40 +12,33 @@
 <body>
     <div class="container">
         <h1 class="headline mt-3">F<span class="rank">rank</span></h1>
-        <div class="row SuperCard shadow">
-            <h3 class="title">Question number #</h3>
-            <input type="text" class="questionInput shadow-sm mb-3" placeholder="Set Question number #">
-            <hr>
-            <h3 class="title">Options</h3>
-            <div class="form-check optionGroup">
-                <input class="form-check-input" type="radio" name="flexRadioDefault">
-                <input type="text" class="optionInput" placeholder="Option 1">
+        <form action="" id="theForm" method="POST">
+            <div class="row SuperCard shadow" id="SuperCard">
+                <h3 class="title">السؤال</h3>
+                <input type="text" class="questionInput shadow-sm mb-3" placeholder="ادخل سؤال" required>
+                <hr>
+                <h3 class="title">الأختيارات</h3>
+                <div id="options">
+                    <div class="form-check optionGroup" id="option1">
+                        <input class="form-check-input" type="radio" name="answerCheck" value="1" required>
+                        <input type="text" class="optionInput" placeholder="ادخل إختيار " required>
+                    </div>
+                    <div class="form-check optionGroup" id="option2">
+                        <input class="form-check-input" type="radio" name="answerCheck" value="2" required>
+                        <input type="text" class="optionInput" placeholder="ادخل إختيار" required>
+                    </div>
+                </div>
+                <input type="button" value="+ إضافة اختيار +" onclick="AddOption()" class="btn btn-primary">
+                <div class="d-flex justify-content-between mt-2">
+                    <input type="submit" value="إضافة سؤال" id="next" class="btn me-3 w-50 btn-primary nextButton">
+                    <input type="button" value="السابق" id="prev" class="btn w-50 btn-primary">
+                </div>
+                <input type="submit" value="إرسال" class="btn btn-primary finish mt-2">
             </div>
-            <div class="form-check optionGroup">
-                <input class="form-check-input" type="radio" name="flexRadioDefault">
-                <input type="text" class="optionInput" placeholder="Option 2">
-            </div>
-            <div class="form-check optionGroup">
-                <input class="form-check-input" type="radio" name="flexRadioDefault">
-                <input type="text" class="optionInput" placeholder="Added Option 3">
-                <input type="button" class="btn cancel" value="X">
-            </div>
-            <input type="text" value="+ Add Option +" class="btn btn-primary">
-            <div class="superCardFooter">
-                <center>
-                    <input type="submit" value="Previous" class="btn btn-primary">
-                    <input type="submit" value="Next" class="btn btn-primary">
-                    <input type="submit" value="Add More Question" class="btn btn-primary">
-                    <input type="submit" value="Delete" class="btn btn-primary">
-                </center>
-            </div>
-        </div>
-        <center>
-            <input type="submit" value="Finish" class="btn btn-primary finish m-3">
-        </center>
-
-    </div>
+        </form>
     </div>
 </body>
+<script src="jquery-3.6.4.min.js"></script>
+<script src="main.js"></script>
 
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'mostafa', '123456', 'frank');
+$conn = mysqli_connect('localhost', 'root', '', 'frank');
 
 
 if (isset($_POST["submit"])) {
@@ -40,13 +40,13 @@ if (isset($_POST["submit"])) {
         </div>
         <form action="index.php" method="POST">
             <div class="row d-flex justify-content-center">
-                <input type="text" class="homeBtn  shadow-sm" placeholder="PLZ Enter your name" name="name">
-                <input class="btn btn-primary w-fit" type="submit" value="Submit" name="submit">
+                <input type="text" class="homeBtn shadow-sm" autocomplete="false" placeholder="PLZ Enter your name"
+                    name="name" required>
+                <input class="btn btn-primary w-50 mt-3" type="submit" value="Let's Start" name="submit">
                 <h1 class="headline m-0">F<span class="rank">rank</span></h1>
             </div>
         </form>
         <div class="row m-1 rounded-1">
-            <?php if(isset($_COOKIE['token'])) :?>
             <table class="table ">
                 <thead>
                     <tr>
@@ -55,36 +55,23 @@ if (isset($_POST["submit"])) {
                         <th>Score</th>
                     </tr>
                 </thead>
-                <?php while ($history = mysqli_fetch_array($result2)): ?>
-                    <tbody>
-                        <tr class="table-warning">
-                            <th class="rank">BFF</th>
-                            <th>
-                                <?php echo $history["guest"]; ?>
-                            </th>
-                            <th>
-                                <?php echo $history["score"]; ?>
-                            </th>
-                        </tr>
-                        <!-- <tr>
-                            <th>2</th>
-                            <th>seadawy 2</th>
-                            <th>seadawy 11</th>
-                        </tr>
-                        <tr>
-                            <th>3</th>
-                            <th>seadawy 3</th>
-                            <th>seadawy 10</th>
-                        </tr>
-                        <tr>
-                            <th>4</th>
-                            <th>seadawy 4</th>
-                            <th>seadawy 5</th>
-                        </tr> -->
-                    </tbody>
-                <?php endwhile; ?>
-                <?php else: echo "sorry you dont seem to have history here yet";
-                endif; ?>
+                <tbody>
+                    <?php if (isset($_COOKIE['token'])): ?>
+                        <?php while ($history = mysqli_fetch_array($result2)): ?>
+                            <tr class="table-warning">
+                                <th class="rank">BFF</th>
+                                <th>
+                                    <?php echo $history["guest"]; ?>
+                                </th>
+                                <th>
+                                    <?php echo $history["score"]; ?>
+                                </th>
+                            </tr>
+                        <?php endwhile; ?>
+                    <?php else:
+                        echo "<tr><th colspan='3'>sorry you dont seem to have history here yet</th></td>";
+                    endif; ?>
+                </tbody>
             </table>
         </div>
     </div>
