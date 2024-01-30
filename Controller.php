508e887e738eb2927125bcc1e5e7d2338e518f45
Mostafa-Maher <sasabolbol424@gmail.com>
2024-01-30 03:34:25 +0200

    inserted

--- Controller.php
+++ Controller.php
@@ -1,4 +1,5 @@
 <?php
+include("db.php");
 if ($_POST['Action'] == "AddQuestion") {
     $Questions = $_POST['Questions'];
     $Options = $_POST['Options'];
@@ -6,4 +7,7 @@ if ($_POST['Action'] == "AddQuestion") {
     echo json_encode($Questions);
     echo json_encode($Options);
     echo json_encode($Answer);
+    $str_options = implode("| ",$Options); 
+    $sql="INSERT INTO questions (title , options , answer) VALUES('$Questions' , '$str_options' , '$Answer')";
+    $query = mysqli_query($conn , $sql);   
 }
