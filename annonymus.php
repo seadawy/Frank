2d37073a1c370ff251ff685f6ad0cd61fb7ce929
Mostafa-Maher <sasabolbol424@gmail.com>
2024-02-01 03:35:01 +0200

    annonymus

--- annonymus.php
+++ annonymus.php
@@ -1,7 +1,11 @@
 <?php
 include("db.php");
 if (isset($_POST['check'])) {
+<<<<<<< HEAD
     $names = ['Frank', 'Franky', 'Fransis', 'Lemon', 'Flippy','Salamander'];
+=======
+    $names = ['Frank', 'Franky', 'Fransis', 'Funky', 'Flippy'];
+>>>>>>> 940cfb7 (annonymus)
     $name = array_rand($names);
     $token = uniqid();
     $sql = "INSERT INTO users (name, token) VALUES ('$name','$token')";
