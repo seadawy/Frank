2646594a0703c99a731388407f681369002bef22
Mostafa-Maher <sasabolbol424@gmail.com>
2024-02-02 05:48:06 +0200

    wrong action page

---
+++ error.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>wrong action</title>  
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/home.css">
+</head>
+
+<body>
+  <h1><span class="rank">nothing to look for here ...</span></h1>
+  <center>
+    <img src="72gi.GIF"width="200px"height="200px">
+ 
+  </center>  
+  <div class="btn btn-primary shadow-sm">
+        <a href="index.php"style="text-decoration:none;color:white;">go back to main page</a>
+    </div>  
+
+
+
+
+
+</body>
+
+</html>
